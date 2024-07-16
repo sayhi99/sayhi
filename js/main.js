48edfcb18dd5ea5a8d7200d3bb40cc29ded268c7
sayhi <tpghk630@naver.com>
2024-07-16 19:49:27 +0900

    edit

--- js/main.js
+++ js/main.js
@@ -78,3 +78,122 @@ function preloadImage(src) {
 function porloadImages(srcs) {
   return Promise.all(srcs.map(preloadImage));
 }
+
+
+//mail wave animation
+
+// const wave2 = document.querySelector(".wave2");
+
+// wave2.innerHTML = wave2.textContent
+//   .split("")
+//   .map((letter, idx) => {
+//     if (letter === " ") return " ";
+//     return `<span style="animation-delay:${
+//       idx * 15
+//     }ms" class="letter">${letter}</span>`;
+//   })
+//   .join("");
+
+//   scroll animation
+// let windowHeight = window.innerHeight
+// const home4EventHandler = ()=>{
+//     const Text = document.querySelector('.home4_textwrap')
+
+
+//     if(Text.getBoundingClientRect().top < windowHeight-200){
+//         setTimeout(()=>{
+
+//         },200) 
+//         window.removeEventListener('scroll',home4EventHandler)
+//     }
+// }
+//  window.addEventListener('scroll',home4EventHandler)
+
+// $(window).scroll(function() {
+// 	var scroll = $(document).scrollTop();
+// 	var minusH = $(document).innerHeight() / 10;
+// 	var target1 = $('.box1').offset().top;
+// 	if (scroll > target1 - minusH) {
+//         const wave2 = document.querySelector(".wave2");
+
+//         wave2.innerHTML = wave2.textContent
+//           .split("")
+//           .map((letter, idx) => {
+//             if (letter === " ") return " ";
+//             return `<span style="animation-delay:${
+//               idx * 15
+//             }ms" class="letter">${letter}</span>`;
+//           })
+//           .join("");
+// 	}
+// });
+
+// const boxes = document.querySelectorAll('.box');
+
+// window.addEventListener('scroll',checkBoxes);
+
+// checkBoxes();
+
+// function checkBoxes(){
+// 	const triggerBottom = window.innerHeight / 5 * 4;
+	
+// 	boxes.forEach(box=>{
+// 		const boxTop = box.getBoundingClientRect().top;
+		
+// 		if(boxTop < triggerBottom) {
+
+//             const wave2 = document.querySelector(".wave2");
+
+//             wave2.innerHTML = wave2.textContent
+//               .split("")
+//               .map((letter, idx) => {
+//                 if (letter === " ") return " ";
+//                 return `<span style="animation-delay:${
+//                   idx * 15
+//                 }ms" class="letter">${letter}</span>`;
+//               })
+//               .join("");
+            
+// 		} else {
+//             const wave2 = document.querySelector(".wave2");
+
+//             wave2.innerHTML = wave2.textContent
+//               .split("")
+//               .map((letter, idx) => {
+//                 if (letter === " ") return " ";
+//                 return `<span style="animation-delay:${
+//                   idx * 15
+//                 }ms">${letter}</span>`;
+//               })
+//               .join("");
+// 		}
+// 	})
+// }
+
+
+const wave2 = document.querySelector(".wave2");
+
+function createLetterSpan(letter, idx) {
+    if (letter === " ") return " ";
+    return `<span style="animation-delay:${idx * 15}ms" class="letter">${letter}</span>`;
+}
+
+function setWaveInnerHTML() {
+    wave2.innerHTML = wave2.textContent.split("").map(createLetterSpan).join("");
+}
+
+setWaveInnerHTML();
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            setWaveInnerHTML();
+        } else {
+            wave2.innerHTML = wave2.textContent; // 초기 상태로 되돌림
+        }
+    });
+}, {
+    threshold: 0.5 // 요소가 50% 이상 보일 때 감지
+});
+
+observer.observe(document.querySelector('.wave2'));
