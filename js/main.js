5c9459762288dc7d9885f1d6482181d0977c8b7c
sayhi <tpghk630@naver.com>
2024-07-17 13:44:29 +0900

    button bover

--- js/main.js
+++ js/main.js
@@ -124,3 +124,35 @@ const observer = new IntersectionObserver(entries => {
 });
 
 observer.observe(document.querySelector('.wave2'));
+
+
+
+// import "./styles.css";
+// import curDot from "cursor-dot";
+
+// const $ = s => document.querySelector(s);
+
+// const cursor = curDot({
+//   easing: 4
+// });
+
+// cursor.over(".title", {
+//   background: "#fff"
+// });
+
+// cursor.over(".react1", {
+//   borderColor: "rgba(255,255,255,.38)"
+// });
+
+// cursor.over(".react2", {
+//   scale: 0.5,
+//   background: "#fff"
+// });
+
+// cursor.over($(".react3"), {
+//   scale: 1.4,
+//   background: "#faa2c1",
+//   borderColor: "transparent"
+// });
+
+// console.log(cursor.parentElement);
